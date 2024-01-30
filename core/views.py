2d6fb300827dc129be74a28a93b39e85c36ed8f8
unknown <alex.naziru.dev@gmail.com>
2024-01-30 16:26:09 +0200

    shop

--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from product.models import Product
 
 
 def frontpage(request):
-    return render(request, "core/frontpage.html")
+    products = Product.objects.all()[0:8]  # This gets the 1st 8 to show on the page from the db
+    return render(request, "core/frontpage.html", {"products": products})
+
+
+def shop(request):
+    products = Product.objects.all()
+
+    return render(request, "core/shop.html", {"products": products})
--- product/models.py
+++ product/models.py
@@ -1,4 +1,31 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
 
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+# Connecting this database to the one above
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    # In description this can remain empty, not a required field
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # This field is to order our products
+    class Meta:
+        ordering = ('-created_at',)
+
+    # This is a string representation of this class
+    def __str__(self):
+        return self.name
