d7f7fd3ec3d42f3dfc4a5d44ec32ab0e6f5603fb
unknown <alex.naziru.dev@gmail.com>
2024-02-11 17:26:43 +0200

    checkout

--- eShop/urls.py
+++ eShop/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views
 from django.urls import path
 from core.views import frontpage, shop, signup
 from product.views import product
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart, checkout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,7 +30,9 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
-    path('cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', cart, name='cart'),
+    path('cart/checkout/', checkout, name='checkout'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('', frontpage, name='frontpage')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Static images config is above
