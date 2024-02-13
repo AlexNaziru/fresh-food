5305a6b1e74ac54be4886de890e260a396458900
unknown <alex.naziru.dev@gmail.com>
2024-02-13 17:41:30 +0200

    edit account

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from product.models import Product, Category
@@ -24,8 +25,24 @@ def signup(request):
     return render(request, "core/signup.html", {"form": form})
 
 
-def login_user(request):
-    return render(request, "core/login.html")
+@login_required
+def myaccount(request):
+
+    return render(request, "core/myaccount.html")
+
+
+@login_required
+def edit_myaccount(request):
+    if request.method == "POST":
+        user = request.user
+        user.first_name = request.POST.get("first_name")
+        user.last_name = request.POST.get("last_name")
+        user.username = request.POST.get("username")
+        user.email = request.POST.get("email")
+        user.save()
+
+        return redirect("myaccount")
+    return render(request, "core/edit_myaccount.html")
 
 
 def shop(request):
