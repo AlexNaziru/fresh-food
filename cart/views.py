910689343047841a2ce67855d37618c6ae1c5666
unknown <alex.naziru.dev@gmail.com>
2024-02-14 17:00:12 +0200

    cart total

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
+from product.models import Product
 from .cart import Cart
 
 
@@ -13,11 +13,42 @@ def add_to_cart(request, product_id):
 
 
 def cart(request):
-
     return render(request, 'cart/cart.html')
 
 
+def update_cart(request, product_id, action): # action for decrementing or incrementing
+    cart = Cart(request)
+    if action == 'increment':
+        cart.add(product_id, 1, True)
+    else:
+        cart.add(product_id, -1, True)
+
+    product = Product.objects.get(pk=product_id)
+    quantity = cart.get_item(product_id)['quantity']
+
+    item = {
+        'product': {
+            'id': product.id,
+            'name': product.name,
+            'image': product.image,
+            'get_thumbnail': product.get_thumbnail(),  # As a function not a property
+            'price': product.price
+        },
+        'total_price': (quantity * product.price) / 100,
+        'quantity': quantity
+    }
+    response = render(request, 'cart/partials/cart_item.html', {'item': item})
+    response['HX-Trigger'] = 'update-menu-cart'
+
+    return response
+
 @login_required
 def checkout(request):
-
     return render(request, 'cart/checkout.html')
+
+
+def hx_menu_cart(request):
+    return render(request, 'cart/menu_cart.html')
+
+def hx_cart_total(request):
+    return render(request, 'cart/partials/cart_total.html')
