935192ed7455e1f00918527cbe5c614dea1d778c
unknown <alex.naziru.dev@gmail.com>
2024-02-24 17:20:33 +0200

    working pay method

--- order/views.py
+++ order/views.py
@@ -1,30 +1,68 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+import json
+import stripe
+from django.conf import settings
+from django.http import JsonResponse
 from .models import Order, OrderItem
 from cart.cart import Cart
 
 
 def start_order(request):
     cart = Cart(request)
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        address = request.POST.get('address')
-        zipcode = request.POST.get('zipcode')
-        place = request.POST.get('place')
-        phone = request.POST.get('phone')
+    # Getting info from the front end
+    data = json.loads(request.body)
+    # Sending price to stripe initially
+    total_price = 0
 
-        order = Order.objects.create(user=request.user, first_name=first_name, last_name=last_name, email=email, address=address,
-                                     zipcode=zipcode, place=place, phone=phone)
-        for item in cart:
-            product = item['product']
-            quantity = int(item['quantity'])
-            price = product.price * quantity
+    items = []
+    for item in cart:
+        product = item['product']
+        total_price += product.price * int(item['quantity'])
 
-            item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
+        # Setting up a dictionary to append to items list
+        obj = {  # Info that stripe needs from us to work
+            'price_data': {
+                'currency': 'ron',
+                'product_data': {
+                    'name': product.name
+                },
+                'unit_amount': product.price,
+            },
+            'quantity': item['quantity']
+        }
+        items.append(obj)
 
-        return redirect('myaccount')
+    # When we loop, we create a session
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    session = stripe.checkout.Session.create(
+        payment_method_types = ['card'],
+        line_items = items,
+        mode = 'payment',
+        success_url = 'http://127.0.0.1:8000/cart/success',
+        cancel_url = 'http://127.0.0.1:8000/cart'
+    )
+    payment_intent = session.payment_intent  # This is an id we get from stripe when everything is ok
 
-    return redirect('cart')
+    first_name = data['first_name']
+    last_name = data['last_name']
+    email = data['email']
+    address = data['address']
+    zipcode = data['zipcode']
+    place = data['place']
+    phone = data['phone']
+
+    order = Order.objects.create(user=request.user, first_name=first_name, last_name=last_name, email=email,
+                                 address=address, zipcode=zipcode, place=place, phone=phone)
+    order.payment_intent = payment_intent
+    order.payment_amount = total_price
+    order.paid = True
+    order.save()
+
+    for item in cart:
+        product = item['product']
+        quantity = int(item['quantity'])
+        price = product.price * quantity
+
+        item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
+
+    return JsonResponse({'session': session, 'order': payment_intent})
 
